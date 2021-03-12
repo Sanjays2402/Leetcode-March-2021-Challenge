70b3ad193ae83aaea2bbe00e3a31de582e699d1b
Sanjay S <sanjays2402@gmail.com>
2021-03-12 23:54:09 +0530

    Create March 12  Solution.cpp

---
+++ March 12/March 12  Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        
+        if (k > s.size()) 
+            return false;
+        
+        unordered_set<string> hash;
+        for(int i=0; i<=s.size()-k; i++)
+        {
+            hash.insert(s.substr(i, k));
+        }
+        return hash.size() == pow(2, k);
+    }
+};
